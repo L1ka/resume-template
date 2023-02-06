69d81d9a62dcde6dd672bd86d9058f0bbb334f3d
L1ka <100157047+L1ka@users.noreply.github.com>
2023-02-06 16:28:34 +0400

    single textarea component

---
+++ src/components/Textarea.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../scss/_textarea.scss";
+
+const Textarea = (props) => {
+  const containerHeight = props.height === "103px" ? "148px" : "224px";
+
+  return (
+    <div className="textarea-container">
+      <label className="textarea-container__label">{props.label}</label>
+      <textarea
+        className="textarea-container__textarea"
+        placeholder={props.placeholder}
+        onChange={(e) => props.handleChange(e)}
+        name={props.name}
+        value={props.value}
+        style={{ height: props.height }}
+      />
+    </div>
+  );
+};
+
+export default Textarea;
